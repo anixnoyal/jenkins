1bd7a0e480c58df6cd4c227ac1016ee26148f019
anixnoyal <anil.noc@gmail.com>
2024-03-20 23:41:31 +0530

    Create jenkins_node_webSocket.sh

---
+++ jenkins_node_webSocket.sh
@@ -0,0 +1,1 @@
+java -jar agent.jar -url https://192.168.1.2 -secret 33455e2f9b48543ad15ea0817ca19ad93109038d3e1e9b04a6479af43d379299 -name anix2 -workDir "/tmp/jenkins" -noCertificateCheck -webSocket
