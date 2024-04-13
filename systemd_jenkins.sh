27919f9239282b9b1ef932820e67f84918a1f792
anixnoyal <anil.noc@gmail.com>
2024-04-13 10:28:36 +0530

    Delete systemd_jenkins.sh

--- systemd_jenkins.sh
+++
@@ -1,4 +0,0 @@
-[Service]
-Environment="JAVA_OPTS=-Xmx1024m"
-Environment="JAVA_OPTS=${JAVA_OPTS} -Djava.awt.headless=true"
-Environment="JAVA_OPTS=${JAVA_OPTS} -Djenkins.install.runSetupWizard=false"
