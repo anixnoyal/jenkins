95511770ece452d7100d1aef49a389565e4cceb9
anixnoyal <anil.noc@gmail.com>
2024-04-13 14:17:26 +0530

    Update Environment_settings.sh

--- Environment_settings.sh
+++ Environment_settings.sh
@@ -1,8 +1,9 @@
 [Service]
 Nice=-19
 Environment="JAVA_OPTS=-Xmx1g -Xms1g -XX:+UseG1GC -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=35 -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+AlwaysPreTouch -Xlog:gc*:file=/var/log/jenkins/gc.log:time,tags:filecount=10,filesize=10M -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/var/log/jenkins/heapdump.hprof"
-Environment="JENKINS_PREFIX=/jenkins2"
 Environment="HOSTNAME=${JENKINS_PREFIX}-`hostname`"
+Environment="JENKINS_PREFIX=/jenkins2"
+Environment="JENKINS_SESSION_TIMEOUT=60"
 Environment="JENKINS_LOG=/var/log/jenkins/jenkins.log"
 # Environment="JENKINS_OPTS=--pluginroot=/var/cache/jenkins/plugins"
 # Environment="JENKINS_HTTPS_PORT=8443"
