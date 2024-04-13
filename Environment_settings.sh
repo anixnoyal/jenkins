3cb604a8b5055f1839636baf504d4e65e4901e92
anixnoyal <anil.noc@gmail.com>
2024-04-13 10:51:15 +0530

    Update Environment_settings.sh

--- Environment_settings.sh
+++ Environment_settings.sh
@@ -1,3 +1,5 @@
 [Service]
 Nice="-19"
-Environment="JAVA_OPTS=-Xmx1g -Xms1g -XX:+UseG1GC -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=35 -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+AlwaysPreTouch -Xlog:gc*:file=/var/log/jenkins/gc.log:time,tags:filecount=10,filesize=10M -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/var/log/jenkins/heapdump.hprof -Djava.util.logging.config.file=/var/lib/jenkins/logging.properties"
+Environment="JAVA_OPTS=-Xmx1g -Xms1g -XX:+UseG1GC -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=35 -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+AlwaysPreTouch -Xlog:gc*:file=/var/log/jenkins/gc.log:time,tags:filecount=10,filesize=10M -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/var/log/jenkins/heapdump.hprof"
+Environment="JENKINS_PREFIX=/jenkins"
+Environment="JENKINS_LOG_PATH=/var/log/jenkins/jenkins.log"
