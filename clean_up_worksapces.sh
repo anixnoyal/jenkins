15012e7a7637bdc4ab24a004debf704ecc726ed3
anixnoyal <anil.noc@gmail.com>
2024-04-16 22:21:17 +0530

    Update clean_up_worksapces.sh

--- clean_up_worksapces.sh
+++ clean_up_worksapces.sh
@@ -1,10 +1,11 @@
-This one uses grep to create a whitelist:
+
+#This one uses grep to create a whitelist:
 
 ls /var/jenkins_home/workspace \ 
   | grep -v -E '(job-to-skip|another-job-to-skip)$' \
   | xargs -I {} rm -rf /var/jenkins_home/workspace/{}
 
-This one uses du and sort to list workspaces in order of largest to smallest. Then, it uses head to grab the first 10
+#This one uses du and sort to list workspaces in order of largest to smallest. Then, it uses head to grab the first 10
 du -d 1 /var/jenkins_home/workspace \
   | sort -n -r \
   | head -n 10 \
