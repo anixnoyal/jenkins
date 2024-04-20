192e2716264051e665298a2ae9d59a20bc09722f
anixnoyal <anil.noc@gmail.com>
2024-04-20 23:18:42 +0530

    Update soft_link_jnhome.sh

--- soft_link_jnhome.sh
+++ soft_link_jnhome.sh
@@ -1,4 +1,4 @@
 
 sudo rsync -avz /var/bli/jenkins/ /jn-home-anix/
-sudo rsync -avz /var/bli/jenkins/ /jn-home-anix/
+sudo mv /var/bli/jenkins /var/bli/jenkins_backup
 sudo ln -s /jn-home-anix /var/bli/jenkins
