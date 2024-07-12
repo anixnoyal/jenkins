1ab9b394c72e827973c93be2ddbd5a118798ee57
anixnoyal <anil.noc@gmail.com>
2024-07-12 21:40:25 +0530

    Update jenkins.log.sh

--- jenkins.log.sh
+++ jenkins.log.sh
@@ -1,15 +1,21 @@
 vi /var/cache/jenkins/logging.properties
 
 handlers=java.util.logging.ConsoleHandler, java.util.logging.FileHandler
+java.util.logging.SimpleFormatter.format = [%1$tF %1$tT][%4$-6s][%2$s] %5$s %6$s %n
 
-
-java.util.logging.FileHandler.level=ALL
 java.util.logging.ConsoleHandler.level = ALL
-# see https://docs.oracle.com/en/java/javase/17/docs/api/java.logging/java/util/logging/SimpleFormatter.html
 
+# handlers=java.util.logging.FileHandler
+java.util.logging.FileHandler.pattern=/var/log/jenkins/jenkins.log
+java.util.logging.FileHandler.formatter=java.util.logging.SimpleFormatter
 
-# Keep this level to ALL or FINEST or it will be filtered before applying other levels
-
-
+#
 # Default level
 .level= INFO
+#.level= ALL
+
+
+##quick config reload
+/bin/systemctl kill -s HUP jenkins
+
+-s HUP: Specifies the signal to send. HUP stands for Hang-Up. When HUP is sent to a process, it typically instructs it to reload its configuration or restart gracefully. For systemd services like Jenkins, this signal often triggers a reload of the service's configuration without fully stopping and starting the service.
