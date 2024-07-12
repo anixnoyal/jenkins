2e486774c1f94f2de7f0bd9590dd99b16715ba5c
anixnoyal <anil.noc@gmail.com>
2024-07-12 21:35:42 +0530

    Update jenkins.log.sh

--- jenkins.log.sh
+++ jenkins.log.sh
@@ -2,23 +2,14 @@ vi /var/cache/jenkins/logging.properties
 
 handlers=java.util.logging.ConsoleHandler, java.util.logging.FileHandler
 
-java.util.logging.ConsoleHandler.level=ALL
-java.util.logging.ConsoleHandler.formatter=java.util.logging.SimpleFormatter
 
 java.util.logging.FileHandler.level=ALL
-java.util.logging.FileHandler.pattern=/var/log/jenkins/jenkins.log
-java.util.logging.FileHandler.limit=50000
-java.util.logging.FileHandler.count=1
-java.util.logging.FileHandler.formatter=java.util.logging.SimpleFormatter
-java.util.logging.FileHandler.append=true
-
-org.eclipse.jetty.server.RequestLog.level=ALL
-org.eclipse.jetty.server.RequestLog.handler=java.util.logging.FileHandler
+java.util.logging.ConsoleHandler.level = ALL
+# see https://docs.oracle.com/en/java/javase/17/docs/api/java.logging/java/util/logging/SimpleFormatter.html
 
 
+# Keep this level to ALL or FINEST or it will be filtered before applying other levels
 
 
-while [ "$(aws ec2 describe-volumes-modifications --volume-ids $VOLUME_ID --query "VolumesModifications[0].ModificationState" --output text)" != "completed" ]; do
-    sleep 10
-    echo -n "."
-done
+# Default level
+.level= INFO
