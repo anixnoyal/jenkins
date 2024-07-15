68867b9bf701fd595af8d3aa8bbd7eac8df12516
anixnoyal <anil.noc@gmail.com>
2024-07-15 09:02:31 +0530

    Update rsync.sh

--- rsync.sh
+++ rsync.sh
@@ -3,3 +3,13 @@
 rsync -avz --compress --links --exclude='/apps/opt/build_dir' --exclude='*.logs' -e "ssh -o StrictHostKeyChecking=no" /apps/opt/ user@remote:/target/directory/opt/
 
 rsync -avz --compress --links --exclude='*.logs' -e "ssh -o StrictHostKeyChecking=no" /apps/opt/build_dir/ user@remote:/target/directory/build_dir/
+
+
+#Generate Checksums on the Source Directory:
+ssh -i /path/to/private_key user@source_host "cd /path/to/source_directory && find . -type f -exec sha256sum {} +" | sort > source_checksums.txt
+
+#Generate Checksums on the Destination Directory:
+ssh -i /path/to/private_key user@remote_host "cd /path/to/destination_directory && find . -type f -exec sha256sum {} +" | sort > destination_checksums.txt
+
+#verify
+diff -u source_checksums.txt destination_checksums.txt
