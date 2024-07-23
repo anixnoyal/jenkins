086abcd33d78b54220ba0a575fc2b643b6bde35c
anixnoyal <anil.noc@gmail.com>
2024-07-23 15:49:57 +0530

    Update threads.sh

--- threads.sh
+++ threads.sh
@@ -1,1 +1,8 @@
+JENKINS_JAVA_OPTIONS="-Djenkins.model.Jenkins.scmPollingThreadCount=20"
+JENKINS_JAVA_OPTIONS="-Dhudson.remoting.Engine.ioThreadCount=20"
+JENKINS_JAVA_OPTIONS="--handlerCountMax=200 --handlerCountMaxIdle=20"
+
+
+# arguments to pass to java
+JENKINS_JAVA_OPTIONS="-Xmx8192m -Djenkins.model.Jenkins.scmPollingThreadCount=50 -Dhudson.remoting.Engine.ioThreadCount=50 --handlerCountMax=200 --handlerCountMaxIdle=20"
 
