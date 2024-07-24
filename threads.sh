6600707629d3a96ed4eeb3c3ba696c49c4312c3b
anixnoyal <anil.noc@gmail.com>
2024-07-24 07:10:31 +0530

    Update threads.sh

--- threads.sh
+++ threads.sh
@@ -7,3 +7,6 @@ JENKINS_JAVA_OPTIONS="--handlerCountMax=200 --handlerCountMaxIdle=20"
 JENKINS_JAVA_OPTIONS="-Xmx8192m -Djenkins.model.Jenkins.scmPollingThreadCount=50 -Dhudson.remoting.Engine.ioThreadCount=50 --handlerCountMax=200 --handlerCountMaxIdle=20"
 
 JAVA_ARGS="-Dhudson.scm.SCM.checkoutRetryCount=5 -Dorg.eclipse.jetty.util.thread.QueuedThreadPool.maxThreads=200 -Dorg.eclipse.jetty.util.thread.QueuedThreadPool.minThreads=20 -Dorg.eclipse.jetty.util.thread.QueuedThreadPool.idleTimeout=60000"
+
+
+println hudson.scm.SCM.all().each { scm -> scm.getDescriptor().checkoutRetryCount }
