9c452a8939253fb9887174137fb3292479659e55
anixnoyal <anil.noc@gmail.com>
2024-09-06 13:14:08 +0530

    Update efs_warm.sh

--- efs_warm.sh
+++ efs_warm.sh
@@ -1,1 +1,11 @@
-find /path/to/efs/mounted/files -type f ! -name "secret.key" -exec grep -Iq . {} \; -and -exec cat {} > /dev/null \;
+find /path/to/efs/mounted/files -type f ! -name "*secret.key*" -atime +7 -exec sh -c '
+  for file; do
+    if grep -Iq . "$file"; then
+      echo "Processing: $file"
+      cat "$file" > /dev/null
+    else
+      echo "Skipping binary file: $file"
+    fi
+  done
+' sh {} +
+#
