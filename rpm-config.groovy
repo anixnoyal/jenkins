4d37a15a21bf6a2c193b3fc8ae637f1d068cdace
anixnoyal <anil.noc@gmail.com>
2025-02-10 11:37:16 +0530

    Update rpm-config.groovy

--- rpm-config.groovy
+++ rpm-config.groovy
@@ -1,25 +1,27 @@
-import org.eclipse.jetty.util.thread.QueuedThreadPool
 import jenkins.model.Jenkins
 
 def server = Jenkins.instance.servletContext.server
-def threadPool = server.getBean(QueuedThreadPool)
+def threadPool = server.getBeans().find { it.class.name.contains("QueuedThreadPool") }
 
-// Get Jetty Thread Pool Details
-def maxThreads = threadPool.getMaxThreads()
-def minThreads = threadPool.getMinThreads()
-def idleTimeout = threadPool.getIdleTimeout()
+if (threadPool) {
+    def maxThreads = threadPool.getMaxThreads()
+    def minThreads = threadPool.getMinThreads()
+    def idleTimeout = threadPool.getIdleTimeout()
 
-// Get Jetty Request Buffer Size
-def requestBufferSize = System.getProperty("org.eclipse.jetty.server.Request.maxFormContentSize", "Default (200KB)")
+    // Get Jetty Request Buffer Size
+    def requestBufferSize = System.getProperty("org.eclipse.jetty.server.Request.maxFormContentSize", "Default (200KB)")
 
-// Check HTTP Keep-Alive
-def keepAlive = System.getProperty("org.eclipse.jetty.server.HttpChannelState.DEFAULT_KEEP_ALIVE", "Not Set (Default: true)")
+    // Check HTTP Keep-Alive
+    def keepAlive = System.getProperty("org.eclipse.jetty.server.HttpChannelState.DEFAULT_KEEP_ALIVE", "Not Set (Default: true)")
 
-// Print Results
-println "===== Jetty Configuration ====="
-println "Jetty Thread Pool Size: ${maxThreads}"
-println "Jetty Min Threads: ${minThreads}"
-println "Jetty Idle Timeout (ms): ${idleTimeout}"
-println "Jetty Request Buffer Size: ${requestBufferSize}"
-println "Jetty Keep-Alive Enabled: ${keepAlive}"
-println "================================"
+    // Print Results
+    println "===== Jetty Configuration ====="
+    println "Jetty Thread Pool Size: ${maxThreads}"
+    println "Jetty Min Threads: ${minThreads}"
+    println "Jetty Idle Timeout (ms): ${idleTimeout}"
+    println "Jetty Request Buffer Size: ${requestBufferSize}"
+    println "Jetty Keep-Alive Enabled: ${keepAlive}"
+    println "================================"
+} else {
+    println "Error: Could not retrieve Jetty thread pool settings. Jetty may not be properly initialized."
+}
