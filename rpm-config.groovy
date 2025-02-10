5654584c40aa949d0e85d0ab7d83d20540dc872c
anixnoyal <anil.noc@gmail.com>
2025-02-10 11:41:28 +0530

    Update rpm-config.groovy

--- rpm-config.groovy
+++ rpm-config.groovy
@@ -1,34 +1,40 @@
 import jenkins.model.Jenkins
 
 def jenkins = Jenkins.instance
-def server = jenkins.servletContext.getServer()
 
-if (server == null) {
-    println "Error: Unable to access Jetty server instance."
-} else {
-    // Find the Jetty thread pool dynamically
-    def threadPool = server.getBeans().find { it.class.name.contains("QueuedThreadPool") }
+try {
+    // Get Jetty Server instance dynamically
+    def server = jenkins.servletContext.getServer() ?: jenkins.servletContext.getAttribute("org.eclipse.jetty.server.Server")
 
-    if (threadPool) {
-        def maxThreads = threadPool.getMaxThreads()
-        def minThreads = threadPool.getMinThreads()
-        def idleTimeout = threadPool.getIdleTimeout()
-
-        // Get Jetty Request Buffer Size
-        def requestBufferSize = System.getProperty("org.eclipse.jetty.server.Request.maxFormContentSize", "Default (200KB)")
-
-        // Check HTTP Keep-Alive
-        def keepAlive = System.getProperty("org.eclipse.jetty.server.HttpChannelState.DEFAULT_KEEP_ALIVE", "Not Set (Default: true)")
-
-        // Print Results
-        println "===== Jetty Configuration ====="
-        println "Jetty Thread Pool Size: ${maxThreads}"
-        println "Jetty Min Threads: ${minThreads}"
-        println "Jetty Idle Timeout (ms): ${idleTimeout}"
-        println "Jetty Request Buffer Size: ${requestBufferSize}"
-        println "Jetty Keep-Alive Enabled: ${keepAlive}"
-        println "================================"
+    if (server == null) {
+        println "Error: Unable to access Jetty server instance. It may not be available in this Jenkins version."
     } else {
-        println "Error: Could not retrieve Jetty thread pool settings. Jetty may not be initialized correctly in this Jenkins instance."
+        // Dynamically find the Jetty thread pool
+        def threadPool = server.getBeans().find { it.class.name.toLowerCase().contains("queuedthreadpool") }
+
+        if (threadPool) {
+            def maxThreads = threadPool.getMaxThreads()
+            def minThreads = threadPool.getMinThreads()
+            def idleTimeout = threadPool.getIdleTimeout()
+
+            // Get Jetty Request Buffer Size
+            def requestBufferSize = System.getProperty("org.eclipse.jetty.server.Request.maxFormContentSize", "Default (200KB)")
+
+            // Check HTTP Keep-Alive
+            def keepAlive = System.getProperty("org.eclipse.jetty.server.HttpChannelState.DEFAULT_KEEP_ALIVE", "Not Set (Default: true)")
+
+            // Print Results
+            println "===== Jetty Configuration ====="
+            println "Jetty Thread Pool Size: ${maxThreads}"
+            println "Jetty Min Threads: ${minThreads}"
+            println "Jetty Idle Timeout (ms): ${idleTimeout}"
+            println "Jetty Request Buffer Size: ${requestBufferSize}"
+            println "Jetty Keep-Alive Enabled: ${keepAlive}"
+            println "================================"
+        } else {
+            println "Error: Could not retrieve Jetty thread pool settings. Jetty may be running differently in this Jenkins version."
+        }
     }
+} catch (Exception e) {
+    println "Error retrieving Jetty settings: ${e.message}"
 }
