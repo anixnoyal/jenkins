e7e470ae2814f5ad06b42bc2a63e50cc7759da51
anixnoyal <anil.noc@gmail.com>
2025-03-07 14:58:13 +0530

    Create cleanup_started_by_timer.groovy

---
+++ cleanup_started_by_timer.groovy
@@ -0,0 +1,23 @@
+import jenkins.model.*
+import hudson.model.*
+import hudson.triggers.TimerTrigger
+
+def jenkins = Jenkins.instance
+def queue = jenkins.queue
+def currentTime = System.currentTimeMillis()
+
+queue.items.each { item ->
+    def causes = item.getCauses()
+
+    // Check if the item was started by a timer
+    def isTimerTriggered = causes.any { it instanceof TimerTrigger.TimerTriggerCause }
+    
+    if (isTimerTriggered) {
+        def waitTimeInSeconds = (currentTime - item.inQueueSince) / 1000 // Convert ms to sec
+        
+        if (waitTimeInSeconds > 60) {
+            println "Removing job: '${item.task.name}' (Waiting: ${waitTimeInSeconds} sec, Started by Timer)"
+            queue.cancel(item)
+        }
+    }
+}
