9b9fa1c74cf44f0dfc40a069b15d477e8c1a57c7
anixnoyal <anil.noc@gmail.com>
2025-03-13 17:11:57 +0530

    Update limit.sh

--- limit.sh
+++ limit.sh
@@ -1,14 +1,6 @@
-def descriptor = Jenkins.get().getDescriptorByType(
-    org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject.DescriptorImpl
-)
-println(descriptor)
-
-
-def descriptor = Jenkins.get().getDescriptorByType(
-    org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject.DescriptorImpl
-)
-println(descriptor.metaClass.methods*.name.sort().unique())
-
 System.setProperty("org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject.concurrentIndexingLimit", "4")
 
 println "Concurrent indexing limit set to: " + System.getProperty("org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject.concurrentIndexingLimit")
+
+println System.getProperty("org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject.concurrentIndexingLimit")
+
