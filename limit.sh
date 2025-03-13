dacdf656e0aa7c9bdc3007b801c05a96892bfb86
anixnoyal <anil.noc@gmail.com>
2025-03-13 17:07:41 +0530

    Update limit.sh

--- limit.sh
+++ limit.sh
@@ -8,3 +8,16 @@ def descriptor = Jenkins.get().getDescriptorByType(
     org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject.DescriptorImpl
 )
 println(descriptor.metaClass.methods*.name.sort().unique())
+
+
+import jenkins.model.Jenkins
+
+def descriptor = Jenkins.get().getDescriptorByType(
+    org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject.DescriptorImpl
+)
+
+def field = descriptor.class.getDeclaredField("concurrentIndexingLimit")
+field.setAccessible(true)
+field.set(descriptor, 4)
+
+println "Concurrent indexing limit set to: " + field.get(descriptor)
