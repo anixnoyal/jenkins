4c100d9cee9cb7501307c0c9efb52283c2d419e8
anixnoyal <anil.noc@gmail.com>
2025-03-13 17:26:24 +0530

    Update limit.sh

--- limit.sh
+++ limit.sh
@@ -4,3 +4,15 @@ println "Concurrent indexing limit set to: " + System.getProperty("org.jenkinsci
 
 println System.getProperty("org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject.concurrentIndexingLimit")
 
+
+-Dorg.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject.concurrentIndexingLimit=100
+
+
+
+import jenkins.util.Timer
+
+int newThreadPoolSize = 100
+Timer.get().setCorePoolSize(newThreadPoolSize)
+
+println "SCM event thread pool size set to: " + Timer.get().getCorePoolSize()
+
