6af109677206bcc7098b70f7909ff73ea7aaf704
anixnoyal <anil.noc@gmail.com>
2025-03-13 18:51:56 +0530

    Update limit.sh

--- limit.sh
+++ limit.sh
@@ -3,3 +3,9 @@ import jenkins.scm.api.SCMEventIndexer
 println "Current Index Scan Pool Size: " + SCMEventIndexer.class.getDeclaredField("poolSize").get(null)
 
 -Djenkins.scm.api.SCMEventIndexer.poolSize=10
+
+System.properties.each { key, value ->
+    if (key.toLowerCase().contains("scm") || key.toLowerCase().contains("index")) {
+        println "$key = $value"
+    }
+}
