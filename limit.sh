503917a06c6900a9d3ce0cb3a999427250f981c6
anixnoyal <anil.noc@gmail.com>
2025-03-13 17:03:46 +0530

    Update limit.sh

--- limit.sh
+++ limit.sh
@@ -2,3 +2,9 @@ def descriptor = Jenkins.get().getDescriptorByType(
     org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject.DescriptorImpl
 )
 println(descriptor)
+
+
+def descriptor = Jenkins.get().getDescriptorByType(
+    org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject.DescriptorImpl
+)
+println(descriptor.metaClass.methods*.name.sort().unique())
