735d8bc00824cddf362f54f5522da3ccd06162b4
anixnoyal <anil.noc@gmail.com>
2025-03-18 06:34:49 +0530

    Update new_sendmail.sh

--- new_sendmail.sh
+++ new_sendmail.sh
@@ -1,5 +1,10 @@
-sed -i -E '/confMAX_DAEMON_CHILDREN/ s/^dnl *//' /etc/mail/sendmail.mc || echo "define(\`confMAX_DAEMON_CHILDREN\', \`100\')dnl" | sudo tee -a /etc/mail/sendmail.mc
-sed -i -E '/confCONNECTION_RATE_THROTTLE/ s/^dnl *//' /etc/mail/sendmail.mc || echo "define(\`confCONNECTION_RATE_THROTTLE\', \`0\')dnl" | sudo tee -a /etc/mail/sendmail.mc
+sed -i '/confMAX_DAEMON_CHILDREN/d' /etc/mail/sendmail.mc
+sed -i '/confCONNECTION_RATE_THROTTLE/d' /etc/mail/sendmail.mc
+
+sudo tee -a /etc/mail/sendmail.mc > /dev/null << 'EOF'
+define(`confMAX_DAEMON_CHILDREN', `100')dnl
+define(`confCONNECTION_RATE_THROTTLE', `0')dnl
+EOF
 
 make -C /etc/mail
 systemctl restart sendmail
